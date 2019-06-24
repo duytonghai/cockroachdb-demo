8a23aacf7b337f9b14da52c36b2dee15862647de
Tam Tran <spkt10110098@gmail.com>
2019-06-24 21:11:28 +0700

    Added endpoints

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,12 +1,49 @@
+var async = require('async');
 var express = require('express');
+var fs = require('fs');
+var pg = require('pg');
 var router = express.Router();
+
+// Connect to the "dev2ta_demo" database.
+var config = {
+    user: process.env.DB_USERNAME || 'maxroach',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'dev2ta_demo',
+    port: process.env.DB_PORT || 26257
+};
+
+// Create a pool.
+var pool = new pg.Pool(config);
 
 /* GET products listing. */
 router.get('/', function(req, res, next) {
-  res.json([
-    {id: '1', name: 'Dawn Professionl Pot And Pan'},
-    {id: '2', name: 'Cake - Bande Of Fruit'}
-  ]);
+  pool.connect(function (err, client, done) {
+    // Close communication with the database and exit.
+    var finish = function () {
+      done();
+      //process.exit();
+    };
+
+    if (err) {
+        console.error('could not connect to cockroachdb', err);
+        finish();
+    }
+    async.waterfall([
+      function (next) {
+        client.query('SELECT id, name, category_id, quantity FROM products;', next);
+      },
+    ],
+    function (err, results) {
+      if (err) {
+        console.error('Error selecting from products: ', err);
+        finish();
+      }
+
+      res.json(results.rows);
+
+      finish();
+    });
+  });
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,86 @@
+var async = require('async');
 var express = require('express');
+var fs = require('fs');
+var pg = require('pg');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource 111');
+// Connect to the "dev2ta_demo" database.
+var config = {
+    user: process.env.DB_USERNAME || 'maxroach',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'dev2ta_demo',
+    port: process.env.DB_PORT || 26257
+};
+
+// Create a pool.
+var pool = new pg.Pool(config);
+
+/* GET user */
+router.get('/:userId', function(req, res, next) {
+  var userId = req.params.userId;
+
+  pool.connect(function (err, client, done) {
+    // Close communication with the database and exit.
+    var finish = function () {
+      done();
+      //process.exit();
+    };
+
+    if (err) {
+        console.error('could not connect to cockroachdb', err);
+        finish();
+    }
+    async.waterfall([
+      function (next) {
+        client.query('SELECT id, first_name, last_name, email, pwd, gender, ip_address, timezone FROM users WHERE users.id =\'' + userId + '\';', next);
+      },
+    ],
+    function (err, results) {
+      if (err) {
+        console.error('Error selecting from users: ', err);
+        finish();
+      }
+
+      res.json(results.rows);
+
+      finish();
+    });
+  });
 });
+
+
+/* GET user's orders */
+router.get('/:userId/orders', function(req, res, next) {
+  var userId = req.params.userId;
+
+  pool.connect(function (err, client, done) {
+    // Close communication with the database and exit.
+    var finish = function () {
+      done();
+      //process.exit();
+    };
+
+    if (err) {
+        console.error('could not connect to cockroachdb', err);
+        finish();
+    }
+    async.waterfall([
+      function (next) {
+        client.query('SELECT * FROM orders WHERE orders.user_id =\'' + userId + '\';', next);
+      },
+    ],
+    function (err, results) {
+      if (err) {
+        console.error('Error selecting from orders: ', err);
+        finish();
+      }
+
+      res.json(results.rows);
+
+      finish();
+    });
+  });
+});
+
 
 module.exports = router;
